43a59e3dcdba54eb760ca2252890c479298fb701
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-07 09:51:11 -0400

    Alphabetize

--- examples/basic_usage/outputs.tf
+++ examples/basic_usage/outputs.tf
@@ -1,20 +1,20 @@
 output "access_key" {
-  value       = module.iam_user_with_ssm_read.access_key
   description = "The IAM access key for the test-ami-build-iam-user-tf-module user."
   sensitive   = true
+  value       = module.iam_user_with_ssm_read.access_key
 }
 
 output "ec2amicreate_role_production" {
-  value       = module.iam_user_with_ssm_read.ec2amicreate_role_production
   description = "The IAM role that allows creation of AMIs in Production by the test-ami-build-iam-user-tf-module user."
+  value       = module.iam_user_with_ssm_read.ec2amicreate_role_production
 }
 
 output "ec2amicreate_role_staging" {
-  value       = module.iam_user_with_ssm_read.ec2amicreate_role_staging
   description = "The IAM role that allows creation of AMIs in Staging by the test-ami-build-iam-user-tf-module user."
+  value       = module.iam_user_with_ssm_read.ec2amicreate_role_staging
 }
 
 output "user" {
-  value       = module.iam_user_with_ssm_read.user
   description = "The test-ami-build-iam-user-tf-module IAM user."
+  value       = module.iam_user_with_ssm_read.user
 }
--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@
 # ------------------------------------------------------------------------------
 
 variable "user_name" {
-  type        = string
   description = "The name to associate with the AWS IAM user (e.g. test-ami-build-iam-user-tf-module)."
+  type        = string
 }
 
 # ------------------------------------------------------------------------------
@@ -16,43 +16,43 @@ variable "user_name" {
 # ------------------------------------------------------------------------------
 
 variable "additional_policy_arns_production" {
-  type        = list(string)
-  description = "The list of additional Production IAM policy ARNs to attach to this IAM user (e.g. [\"arn:aws:iam::123456789012:policy/ReadFromMyBucket\", \"arn:aws:iam::123456789012:policy/ReadFromMyOtherBucket\"])."
   default     = []
+  description = "The list of additional Production IAM policy ARNs to attach to this IAM user (e.g. [\"arn:aws:iam::123456789012:policy/ReadFromMyBucket\", \"arn:aws:iam::123456789012:policy/ReadFromMyOtherBucket\"])."
+  type        = list(string)
 }
 
 variable "additional_policy_arns_staging" {
-  type        = list(string)
-  description = "The list of additional Staging IAM policy ARNs to attach to this IAM user (e.g. [\"arn:aws:iam::123456789012:policy/ReadFromMyBucket\", \"arn:aws:iam::123456789012:policy/ReadFromMyOtherBucket\"])."
   default     = []
+  description = "The list of additional Staging IAM policy ARNs to attach to this IAM user (e.g. [\"arn:aws:iam::123456789012:policy/ReadFromMyBucket\", \"arn:aws:iam::123456789012:policy/ReadFromMyOtherBucket\"])."
+  type        = list(string)
 }
 
 variable "ec2amicreate_policy_name" {
-  type        = string
-  description = "The name of the IAM policy in the Images account that allows all of the actions needed to create an AMI."
   default     = "EC2AMICreate"
+  description = "The name of the IAM policy in the Images account that allows all of the actions needed to create an AMI."
+  type        = string
 }
 
 variable "ec2amicreate_role_description" {
-  type        = string
-  description = "The description to associate with the IAM role that allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
   default     = "Allows the %s IAM user to create AMIs."
+  description = "The description to associate with the IAM role that allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  type        = string
 }
 
 variable "ec2amicreate_role_max_session_duration" {
-  type        = number
-  description = "The maximum session duration (in seconds) when assuming the IAM role that allows this IAM user to create AMIs."
   default     = 3600
+  description = "The maximum session duration (in seconds) when assuming the IAM role that allows this IAM user to create AMIs."
+  type        = number
 }
 
 variable "ec2amicreate_role_name" {
-  type        = string
-  description = "The name to assign the IAM role that allows allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
   default     = "EC2AMICreate-%s"
+  description = "The name to assign the IAM role that allows allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  type        = string
 }
 
 variable "ssm_parameters" {
-  type        = list(string)
-  description = "The AWS SSM parameters that the IAM user needs to be able to read (e.g. [\"/example/parameter1\", \"/example/config\"])."
   default     = []
+  description = "The AWS SSM parameters that the IAM user needs to be able to read (e.g. [\"/example/parameter1\", \"/example/config\"])."
+  type        = list(string)
 }
